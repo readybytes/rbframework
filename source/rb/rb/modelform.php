5223cf7ffad2e383c9a90b3a93511bd3ff2a543e
Gaurav Jain <gaurav@readybytes.in>
2013-05-13 11:26:14 +0530

    bug fixed of usin undefined constant JPATH_COMPONENT in modelform

--- source/rb/rb/modelform.php
+++ source/rb/rb/modelform.php
@@ -77,4 +77,63 @@ abstract class Rb_Modelform extends JModelForm
 		return $this;
 	}
 	
+	/**
+	 * over-rided because it uses JPATH_COMPONENT constant, without checking
+	 * (non-PHPdoc)
+	 * @see libraries/legacy/model/JModelForm::loadForm()
+	 */
+	protected function loadForm($name, $source = null, $options = array(), $clear = false, $xpath = false)
+	{
+		// Handle the optional arguments.
+		$options['control'] = JArrayHelper::getValue($options, 'control', false);
+
+		// Create a signature hash.
+		$hash = md5($source . serialize($options));
+
+		// Check if we can use a previously loaded form.
+		if (isset($this->_forms[$hash]) && !$clear)
+		{
+			return $this->_forms[$hash];
+		}
+
+		// Get the form.
+		// check before using
+		if(defined('JPATH_COMPONENT')){
+			JForm::addFormPath(JPATH_COMPONENT . '/models/forms');
+			JForm::addFieldPath(JPATH_COMPONENT . '/models/fields');
+		}
+
+		try
+		{
+			$form = JForm::getInstance($name, $source, $options, false, $xpath);
+
+			if (isset($options['load_data']) && $options['load_data'])
+			{
+				// Get the data for the form.
+				$data = $this->loadFormData();
+			}
+			else
+			{
+				$data = array();
+			}
+
+			// Allow for additional modification of the form, and events to be triggered.
+			// We pass the data because plugins may require it.
+			$this->preprocessForm($form, $data);
+
+			// Load the data into the form after the plugins have operated.
+			$form->bind($data);
+
+		}
+		catch (Exception $e)
+		{
+			$this->setError($e->getMessage());
+			return false;
+		}
+
+		// Store the form for later.
+		$this->_forms[$hash] = $form;
+
+		return $form;
+	}
 }
