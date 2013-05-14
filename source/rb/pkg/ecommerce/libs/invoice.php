9a3d25214e89e3ea74ccf012c96ee17200ae57f4
Manisha Ranawat <manisha@readybytes.in>
2013-05-14 10:40:28 +0530

    Issue fix while creating invoice, paid and refund date will be shown when invoice status paid or refund

--- source/rb/pkg/ecommerce/libs/invoice.php
+++ source/rb/pkg/ecommerce/libs/invoice.php
@@ -103,8 +103,8 @@ class Rb_EcommerceInvoice extends Rb_EcommerceLib
 		$this->params 				= '';
 		$this->created_date 		= new Rb_Date();
 		$this->modified_date 		= new Rb_Date();
-		$this->paid_date 			= new Rb_Date();
-		$this->refund_date			= new Rb_Date();
+		$this->paid_date 			= null;
+		$this->refund_date			= null;
 		$this->due_date 			= new Rb_Date();
 		$this->issue_date 			= new Rb_Date();
 		$this->processor_type 		= '';
