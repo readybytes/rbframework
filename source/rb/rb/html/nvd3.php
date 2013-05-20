428d53abd1949b09b0823b85bbce049e13224c17
Gaurav Jain <gaurav@readybytes.in>
2013-05-20 15:41:24 +0530

    nvd3 lib updated

---
+++ source/rb/rb/html/nvd3.php
@@ -0,0 +1,30 @@
+<?php
+/**
+* @copyright	Copyright (C) 2009 - 2012 Ready Bytes Software Labs Pvt. Ltd. All rights reserved.
+* @license		GNU/GPL, see LICENSE.php
+* @package		RB Framework
+* @subpackage	Frontend
+* @contact 		payplans@readybytes.in
+*/
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * @author Gaurav Jain
+ */
+abstract class Rb_HtmlNvd3
+{
+	public static function load($attribs = array())
+	{
+		static $loaded = false;
+		
+		if($loaded === false){
+			$loaded = true;
+			Rb_Html::stylesheet('nvd3/nv.d3.css', $attribs, false);
+			
+			Rb_Html::script('nvd3/d3.v3.js', $attribs, false);
+			Rb_Html::script('nvd3/nv.d3.js', $attribs, false);
+			
+		}
+	}
+}
