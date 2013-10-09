9a8685689e8d786af38b3cd696b5b12609be0e99
Shyam Sunder Verma <shyam@readybyts.in>
2013-10-09 11:01:01 +0530

    Tax System for eCommerce Package

---
+++ source/rb/pkg/ecommerce/tax/request.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+* @copyright	Copyright (C) 2009 - 2012 Ready Bytes Software Labs Pvt. Ltd. All rights reserved.
+* @license		GNU/GPL, see LICENSE.php
+* @package 		Rb_Ecommerce
+* @subpackage	Front-end
+* @contact		team@readybytes.in
+*/
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+/** 
+ * Tax Request Base Class
+ * @author Shyam Sunder Verma
+ */
+class Rb_EcommerceTaxRequest extends Rb_Registry
+{
+	/* Stores
+	 *  1. tax_rate
+	 *  2. taxable_amount
+	 *  3. buyer_country, buyer_state, buyer_city, buyer_zipcode , buyer_tin, buyer_purpose
+	 *  4. seller_country, seller_state, seller_city, seller_zipcode , seller_tin
+	 *  */
+}
