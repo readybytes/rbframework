16374c913e7439aec7e6f9421b2d8bfef70dc12d
Neelam <neelam@readybytes.in>
2015-07-28 17:06:27 +0530

    Fix::Fixes strict standard issue in Rb_EcommerceRequest set() function
    Ref #62

--- source/rb/pkg/ecommerce/payment/request.php
+++ source/rb/pkg/ecommerce/payment/request.php
@@ -11,38 +11,12 @@
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
-/** 
- * Request Base Class
- * @author Gaurav Jain
- */
-class Rb_EcommerceRequest extends Rb_Registry
-{
-	const BUILD_TYPE_XML	=	'xml';
-	const BUILD_TYPE_HTML	=	'html';	
-	/**
-	* Constructor
-	*
-	* @param mixed $data The data to bind to the new RB_Registry object.
-	*
-	* @since 11.1
-	*/
-	public function __construct($data = null)
-	{
-		// Construct JRegistry
-		parent::__construct($data);
-		
-//		// Set default data
-//		$this->data->regular_price 	= 0;
-//		$this->data->trial_prices 	= array();
-//		$this->data->trial_times		= array();
-//		$this->data->is_recurring 	= false;
-//		$this->data->item_name		= '';		
-//		$this->data->currency 		= 'USD'; //XITODO : get default currency from configuration
-	}
-	
-	public function set($path, $value)
-	{
-		parent::set($path, $value);		
-		return $this;
-	}
+if(RB_CMS_ECOM_ADAPTER==='j33'){
+	class Rb_EcommerceRequest extends Rb_EcommerceAdaptJ33Request
+	{}
+}
+
+if(RB_CMS_ECOM_ADAPTER==='j35'){
+	class Rb_EcommerceRequest extends Rb_EcommerceAdaptJ35Request
+	{}
 }
